ad3145b26857b488ce7ec0f7c5f03cd7ec67f690
Andreu Julià lliteras <andreujulia@estudiante.edib.es>
2024-01-16 20:44:01 +0100

    Index terminado y ajustes empezado

--- js/procesa.js
+++ js/procesa.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", function () {
   let boton_encendido = document.getElementById("boton_encendido");
-  let aguja_movimiento = document.querySelector(".aguja");
+  let aguja_movimiento = document.getElementById("aguja");
+
+  // Si quisieramos cogerla por su clase en vez de por su id ponedríamos let aguja_movimiento = document.querySelector(".aguja");
 
   boton_encendido.addEventListener("click", function () {
     // Cuando le hago click en el boton de encendido/apagado de pone y se quita con toggle la clase moviemiento.
